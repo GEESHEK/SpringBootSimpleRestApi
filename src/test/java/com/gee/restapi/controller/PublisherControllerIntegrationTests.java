efa1487a9bc7c0cfdd341bf41d12f63b6a1a3db8
Gee Chong Shek <geeshek95@outlook.com>
2025-07-22 19:24:09 +0100

    Publisher Controller Tested

--- src/test/java/com/gee/restapi/controller/PublisherControllerIntegrationTests.java
+++ src/test/java/com/gee/restapi/controller/PublisherControllerIntegrationTests.java
@@ -1,7 +1,9 @@
 package com.gee.restapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gee.restapi.model.entity.Game;
 import com.gee.restapi.model.entity.Publisher;
+import com.gee.restapi.model.request.PublisherRequest;
 import com.gee.restapi.respository.PublisherRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,9 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,19 +37,22 @@ public class PublisherControllerIntegrationTests {
 
     @BeforeEach
     public void setup() {
-        Publisher publisher1 = new Publisher();
-        publisher1.setName("Test Publisher1");
-//        Publisher publisher2 = new Publisher();
-//        publisher2.setName("Test Publisher2");
-//        Publisher publisher3 = new Publisher();
-//        publisher3.setName("Test Publisher3");
-        publisherRepository.save(publisher1);
-//        publisherRepository.save(publisher2);
-//        publisherRepository.save(publisher3);
+        Publisher publisher = new Publisher();
+        publisher.setName("Test Publisher1");
+
+        Game game = new Game();
+        game.setName("Test Game1");
+        game.setPublisher(publisher); // must set the publisher on the key, not as smart as EF
+
+        List<Game> games = new ArrayList<>();
+        games.add(game);
+        publisher.setGames(games);
+
+        publisherRepository.save(publisher);
     }
 
     @Test
-    public void testFindAllPublishersReturnHttpStatus200AndPublishers() throws Exception {
+    public void testFindAllPublishersReturnsHttpStatus200AndListOfPublishers() throws Exception {
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/v1/publishers")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -55,4 +63,99 @@ public class PublisherControllerIntegrationTests {
         );
     }
 
+    @Test
+    public void testFindPublisherByIdReturnsHttpStatus200AndPublisher() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/v1/publishers/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("$.id").value(1),
+                MockMvcResultMatchers.jsonPath("$.name").value("Test Publisher1")
+        );
+    }
+
+//    @GetMapping("/{id}/with-games")
+//    public PublisherWithGamesDto getPublisherWithGames(@PathVariable Long id) {
+//        return publisherService.getPublisherWithGames(id);
+//    }
+
+    @Test
+    public void testFindPublisherByIdReturnsHttpStatus200AndPublisherIncludingGames() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/v1/publishers/1/with-games")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("$.id").value(1),
+                MockMvcResultMatchers.jsonPath("$.name").value("Test Publisher1"),
+                MockMvcResultMatchers.jsonPath("$.games[0].name").value("Test Game1"),
+                MockMvcResultMatchers.jsonPath("$.games.length()").value(1)
+        );
+    }
+
+    @Test
+    public void testFindPublisherByIdReturnsHttpStatus201AndSavedPublisher() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/v1/publishers/99")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isNotFound()
+        );
+    }
+
+    @Test
+    public void testCreatePublisherReturnsHttpStatus201WhenPublisherDoesNotExist() throws Exception {
+        Publisher testPublisher = new Publisher();
+        testPublisher.setName("Tester");
+        String json = objectMapper.writeValueAsString(testPublisher);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/publishers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isCreated(),
+                MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+                MockMvcResultMatchers.jsonPath("$.name").value(testPublisher.getName())
+        );
+    }
+
+    @Test
+    public void testThatUpdatePublisherByIdReturnsHttpStatus200WhenPublisherExist() throws Exception {
+        PublisherRequest publisherRequest = new PublisherRequest();
+        publisherRequest.setName("Updated Publisher");
+        String json = objectMapper.writeValueAsString(publisherRequest);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/api/v1/publishers/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+                MockMvcResultMatchers.jsonPath("$.name").value(publisherRequest.getName())
+        );
+    }
+
+    @Test
+    public void testThatDeletePublisherByIdReturnsHttpStatus404WhenPublisherDoesNotExist() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/publishers/99")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isNotFound()
+        );
+    }
+
+    @Test
+    public void testThatDeletePublisherByIdReturnsHttpStatus204WhenPublisherExist() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/publishers/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpectAll(
+                MockMvcResultMatchers.status().isNoContent()
+        );
+    }
 }
